7dcd3cc23ef17cc8589cd735c0dd24156be2c010
starwalker <juan.rodriguez2803@hotmail.com>
2023-03-29 19:44:09 -0600

    commit spring boot api

---
+++ src/main/java/com/linux/HelloWorldServerApplication.java
@@ -0,0 +1,13 @@
+package com.linux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/linux/controller/Test.java
@@ -0,0 +1,16 @@
+package com.linux.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+	
+	@RequestMapping(value = "/hello", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
+	public String hello() {
+		return "Hello World from Ubuntu 16...";
+	}
+
+}
